78f46f345fd10f7941037c3ee55823824e6fe349
MHarg <61030196+MHarg@users.noreply.github.com>
2020-02-13 17:06:01 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,28 @@
-A
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class Main
+{
+    // instance variables - replace the example below with your own
+    private static String quit = "quit"; 
+    private static String question = "";
+
+    /**
+     * Constructor for objects of class Main
+     */
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        while ((quit.equals(question)!=true)){
+            question = scan.nextLine();
+            System.out.println (question);
+
+        }
+    }
+
+}
